41f754f8ccddd4e943e610a45823052bc7758003
Piotr Komborski <piotr.komborski@digital.cabinet-office.gov.uk>
2016-09-20 14:01:27 +0100

    GetTag method + tests
    This method can retrieve a given tag from RDS DB instance. It will be used to check if skip_final_snapshot was set during provision phase.

--- awsrds/rds_db_instance.go
+++ awsrds/rds_db_instance.go
@@ -101,6 +101,55 @@ func (r *RDSDBInstance) DescribeByTag(tagKey, tagValue string) ([]*DBInstanceDet
 	return dbInstanceDetails, nil
 }
 
+func (r *RDSDBInstance) GetTag(ID, tagKey string) (string, error) {
+
+	describeDBInstancesInput := &rds.DescribeDBInstancesInput{
+		DBInstanceIdentifier: aws.String(ID),
+	}
+
+	r.logger.Debug("get-tag", lager.Data{"input": describeDBInstancesInput})
+
+	dbInstances, err := r.rdssvc.DescribeDBInstances(describeDBInstancesInput)
+	if err != nil {
+		r.logger.Error("aws-rds-error", err)
+		if awsErr, ok := err.(awserr.Error); ok {
+			if reqErr, ok := err.(awserr.RequestFailure); ok {
+				if reqErr.StatusCode() == 404 {
+					return "", ErrDBInstanceDoesNotExist
+				}
+			}
+			return "", errors.New(awsErr.Code() + ": " + awsErr.Message())
+		}
+		return "", err
+	}
+
+	for _, dbInstance := range dbInstances.DBInstances {
+		dbArn, err := r.dbInstanceARN(*dbInstance.DBInstanceIdentifier)
+
+		if err != nil {
+			return "", err
+		}
+
+		listTagsForResourceInput := &rds.ListTagsForResourceInput{
+			ResourceName: aws.String(dbArn),
+		}
+
+		listTagsForResourceOutput, err := r.rdssvc.ListTagsForResource(listTagsForResourceInput)
+
+		if err != nil {
+			return "", err
+		}
+
+		for _, t := range listTagsForResourceOutput.TagList {
+			if *t.Key == tagKey {
+				return *t.Value, nil
+			}
+		}
+	}
+
+	return "", nil
+}
+
 func (r *RDSDBInstance) Create(ID string, dbInstanceDetails DBInstanceDetails) error {
 	createDBInstanceInput := r.buildCreateDBInstanceInput(ID, dbInstanceDetails)
 
