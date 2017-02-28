40441087dcd73f8a416e84159854b43741a28706
Colin Saliceti <c.saliceti@gmail.com>
2017-02-28 11:35:40 +0000

    Ignore drop non existing user
    This will allow tenants with old binds to unbind. Because when
    unbinding, the broker drops the user. But this user doesn't exist if
    this is an old bind.

--- sqlengine/postgres_engine.go
+++ sqlengine/postgres_engine.go
@@ -121,6 +121,10 @@ func (d *PostgresEngine) DropUser(bindingID string) error {
 	dropUserStatement := fmt.Sprintf(`drop role "%s"`, username)
 
 	if _, err := d.db.Exec(dropUserStatement); err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "42704" {
+			d.logger.Info("warning", lager.Data{"warning": "User " + username + " does not exist"})
+			return nil
+		}
 		d.logger.Error("sql-error", err)
 		return err
 	}
