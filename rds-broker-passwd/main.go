e134ae1bc30a650c0c260af4acef90c8abb337f0
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2017-08-03 14:16:18 +0100

    allow passing inputs as cli flags
    I wanted to pass in the "seed" and "uuid" arguments as cli flags rather than interactive input so I can use in a little development script.

--- rds-broker-passwd/main.go
+++ rds-broker-passwd/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/alphagov/paas-rds-broker/rdsbroker"
 	"github.com/alphagov/paas-rds-broker/utils"
 )
 
+var (
+	seed = flag.String("seed", "", "master password seed")
+	uuid = flag.String("id", "", "rds instance uuid")
+)
+
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	var seed, instance string
+	if seed == nil || *seed == "" {
+		fmt.Printf("master password seed: ")
+		fmt.Scanln(seed)
+		fmt.Printf("\n")
+	}
+	if uuid == nil || *uuid == "" {
+		fmt.Printf("instance uuid: ")
+		fmt.Scanln(uuid)
+		fmt.Printf("\n")
+	}
 
-	fmt.Printf("master password seed: ")
-	fmt.Scanln(&seed)
-	fmt.Printf("instance uuid: ")
-	fmt.Scanln(&instance)
-
-	fmt.Printf("\n%s\n", utils.GetMD5B64(seed+instance, rdsbroker.MasterPasswordLength))
+	fmt.Println(utils.GetMD5B64(*seed+*uuid, rdsbroker.MasterPasswordLength))
 }
