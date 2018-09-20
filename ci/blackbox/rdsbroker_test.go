dc5e01d48ec8d2c719cf557fdb4e864f15aae0b6
Hector Rivas Gandara <keymon@gmail.com>
2018-09-20 15:57:25 +0100

    Revert "Focus on the simplest test"
    This reverts commit de15d01da4e35394b34242d0d36cd9b1306da439.

--- ci/blackbox/rdsbroker_test.go
+++ ci/blackbox/rdsbroker_test.go
@@ -203,7 +203,7 @@ var _ = Describe("RDS Broker Daemon", func() {
 			})
 		}
 
-		FDescribe("Postgres", func() {
+		Describe("Postgres", func() {
 			TestProvisionBindDeprovision("postgres")
 		})
 
