922f1254c73b3048e0c28550069393bb97145100
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2019-02-22 14:09:07 +0000

    Actually integration test MySQL
    Service plans require a globally unique ID, but the integration test config had the same plan id's for both MySQL and Postgres, which resulted in tests marked as testing MySQL picking a Postgres plan. This commit gives them suitably unique ids.

--- ci/blackbox/rdsbroker_test.go
+++ ci/blackbox/rdsbroker_test.go
@@ -202,15 +202,15 @@ var _ = Describe("RDS Broker Daemon", func() {
 		}
 
 		Describe("Postgres 9.5", func() {
-			TestProvisionBindDeprovision("postgres", "micro-without-snapshot")
+			TestProvisionBindDeprovision("postgres", "postgres-micro-without-snapshot")
 		})
 
 		Describe("Postgres 10.5", func() {
-			TestProvisionBindDeprovision("postgres", "micro-without-snapshot-10")
+			TestProvisionBindDeprovision("postgres", "postgres-micro-without-snapshot-10")
 		})
 
 		Describe("MySQL", func() {
-			TestProvisionBindDeprovision("mysql", "micro-without-snapshot")
+			TestProvisionBindDeprovision("mysql", "mysql-micro-without-snapshot")
 		})
 	})
 
@@ -310,15 +310,15 @@ var _ = Describe("RDS Broker Daemon", func() {
 		}
 
 		Describe("Postgres 9.5", func() {
-			TestFinalSnapshot("postgres", "micro")
+			TestFinalSnapshot("postgres", "postgres-micro")
 		})
 
 		Describe("Postgres 10.5", func() {
-			TestFinalSnapshot("postgres", "micro-10")
+			TestFinalSnapshot("postgres", "postgres-micro-10")
 		})
 
 		Describe("MySQL", func() {
-			TestFinalSnapshot("mysql", "micro")
+			TestFinalSnapshot("mysql", "mysql-micro")
 		})
 	})
 
@@ -429,15 +429,15 @@ var _ = Describe("RDS Broker Daemon", func() {
 		}
 
 		Describe("Postgres 9.5", func() {
-			TestRestoreFromSnapshot("postgres", "micro")
+			TestRestoreFromSnapshot("postgres", "postgres-micro")
 		})
 
 		Describe("Postgres 10.5", func() {
-			TestRestoreFromSnapshot("postgres", "micro-10")
+			TestRestoreFromSnapshot("postgres", "postgres-micro-10")
 		})
 
 		Describe("MySQL", func() {
-			TestRestoreFromSnapshot("mysql", "micro")
+			TestRestoreFromSnapshot("mysql", "mysql-micro")
 		})
 	})
 
