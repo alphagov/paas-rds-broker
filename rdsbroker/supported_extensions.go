8c6262c18682f6af2f2e390b942e059bc3f03d96
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2021-08-25 14:19:35 +0100

    supported extensions: add entries for postgres13

--- rdsbroker/supported_extensions.go
+++ rdsbroker/supported_extensions.go
@@ -8,7 +8,37 @@ type DBExtension struct {
 // Lists the supported database extensions
 // that require libraries to be loaded on startup,
 // keyed on database engine family
+//
+// Note that not all of these extensions necessarily
+// make sense to allow in the offering configuration
 var SupportedPreloadExtensions = map[string][]DBExtension{
+	"postgres13": {
+		DBExtension{
+			Name:                   "orafce",
+			RequiresPreloadLibrary: true,
+		},
+		DBExtension{
+			Name:                   "pgaudit",
+			RequiresPreloadLibrary: true,
+		},
+		DBExtension{
+			Name:                   "pglogical",
+			RequiresPreloadLibrary: true,
+		},
+		DBExtension{
+			Name:                   "pg_similarity",
+			RequiresPreloadLibrary: true,
+		},
+		DBExtension{
+			Name:                   "pg_stat_statements",
+			RequiresPreloadLibrary: true,
+		},
+		DBExtension{
+			Name:                   "pg_hint_plan",
+			RequiresPreloadLibrary: true,
+		},
+	},
+
 	"postgres12": {
 		DBExtension{
 			Name:                   "auto_explain",
