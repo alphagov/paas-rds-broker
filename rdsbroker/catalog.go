3e2fd180dad3e58302049962ec4d709546e75b3b
Ben Corlett <ben.corlett@digital.cabinet-office.gov.uk>
2022-11-18 12:33:47 +0000

    Add Maintenance info

--- rdsbroker/catalog.go
+++ rdsbroker/catalog.go
@@ -2,10 +2,11 @@ package rdsbroker
 
 import (
 	"fmt"
-	"github.com/Masterminds/semver"
-	"github.com/pivotal-cf/brokerapi/v8/domain"
 	"regexp"
 	"strings"
+
+	"github.com/Masterminds/semver"
+	"github.com/pivotal-cf/brokerapi/v8/domain"
 
 	"github.com/pivotal-cf/brokerapi/v8"
 )
@@ -28,24 +29,30 @@ type CatalogExternal struct {
 }
 
 type Service struct {
-	ID              string                            `json:"id"`
-	Name            string                            `json:"name"`
-	Description     string                            `json:"description"`
-	Tags            []string                          `json:"tags,omitempty"`
-	PlanUpdatable   bool                              `json:"plan_updateable"`
-	Plans           []ServicePlan                     `json:"plans"`
+	ID              string                         `json:"id"`
+	Name            string                         `json:"name"`
+	Description     string                         `json:"description"`
+	Tags            []string                       `json:"tags,omitempty"`
+	PlanUpdatable   bool                           `json:"plan_updateable"`
+	Plans           []ServicePlan                  `json:"plans"`
 	Requires        []domain.RequiredPermission    `json:"requires,omitempty"`
 	Metadata        *domain.ServiceMetadata        `json:"metadata,omitempty"`
 	DashboardClient *domain.ServiceDashboardClient `json:"dashboard_client,omitempty"`
 }
 
 type ServicePlan struct {
-	ID            string                         `json:"id"`
-	Name          string                         `json:"name"`
-	Description   string                         `json:"description"`
-	Free          *bool                          `json:"free,omitempty"`
-	Metadata      *brokerapi.ServicePlanMetadata `json:"metadata,omitempty"`
-	RDSProperties RDSProperties                  `json:"rds_properties,omitempty"`
+	ID              string                         `json:"id"`
+	Name            string                         `json:"name"`
+	Description     string                         `json:"description"`
+	Free            *bool                          `json:"free,omitempty"`
+	Metadata        *brokerapi.ServicePlanMetadata `json:"metadata,omitempty"`
+	MaintenanceInfo *MaintenanceInfo               `json:"maintenance_info,omitempty"`
+	RDSProperties   RDSProperties                  `json:"rds_properties,omitempty"`
+}
+
+type MaintenanceInfo struct {
+	Version     string `json:"version,omitempty"`
+	Description string `json:"description,omitempty"`
 }
 
 type RDSProperties struct {
